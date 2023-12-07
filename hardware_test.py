c7f65057112486c003ba97fda39234df07da39cb
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-12-07 14:18:23 -0500

    Simplify hardware_test for LCD test only

--- hardware_test.py
+++ hardware_test.py
@@ -1,6 +1,7 @@
 from gpiozero import Button, LED
 from time import sleep
 from I2C_LCD_driver import lcd
+'''
 LED_pins = [14, 15, 18, 23, 24, 25]
 adder_pins = [9, 11, 0, 5, 6, 13, 19, 25]
 button_pins = [20, 21]
@@ -8,7 +9,7 @@ button_pins = [20, 21]
 LED_objects = [LED(pin) for pin in LED_pins]
 adder_objects = [LED(pin) for pin in adder_pins]
 button_objects = [Button(pin) for pin in button_pins]
-
+'''
 screen = lcd()
 
 def test_display_string():
@@ -38,10 +39,10 @@ def test_backlight():
     screen.backlight(0)
     sleep(5)  # Wait 5 seconds to visually check the backlight
 
-
+'''
 def button_press():
     print("Button pressed at pin!")
-'''
+
 for signal in adder_objects:
     signal.on()
     sleep(1)
@@ -56,7 +57,7 @@ for light in LED_objects:
 test_backlight()
 test_clear_display()
 test_display_string()
-
+'''
 for button in button_objects:
     button.when_pressed = button_press(button.pin.number)
 
@@ -73,4 +74,4 @@ for button in button_objects:
         button.wait_for_press()
         light.off()
         sleep(1)
-    
+'''
